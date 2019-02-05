b0ab37c528e821b6f465e3846938bb1d31732766
Evan Agee <evanagee@gmail.com>
2019-02-05 08:59:00 -0600

    Removed reference to Bootstrap Vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
     <h3>Welcome!</h3>
-    <p>You'll probably want to remove all of this stuff, but hey it at least gives you a preview of Bootstrap-Vue!</p>
-    <p>I would encourage you to avoid using jQuery and instead find the Vue way of doing some things. You'll thank me later!</p>
+    <p>You'll probably want to remove all of this stuff, but hey it at least it works!</p>
     <p>
       ~
-      <a href="http://evanagee.com/">Evan Agee</a>
+      <a href="https://evanagee.com/">Evan Agee</a>
     </p>
 
     <recent-posts-widget limit="5">Recent Posts</recent-posts-widget>
