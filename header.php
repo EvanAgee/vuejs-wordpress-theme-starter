50172dff789c691c7903c176b6128131c64a6622
Osama Abbas <oabbas2512@gmail.com>
2019-08-05 19:50:22 +0200

    Divided index.php (#64)

---
+++ header.php
@@ -0,0 +1,13 @@
+<?php status_header(200); ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+
+  <head>
+    <meta
+      charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <?php wp_head(); ?>
+  </head>
+
+  <body <?php body_class(); ?>>
--- index.php
+++ index.php
@@ -1,14 +1,5 @@
-<?php status_header(200); ?>
-<!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js no-svg">
-<head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="profile" href="http://gmpg.org/xfn/11">
-  <?php wp_head(); ?>
-</head>
-<body>
-  <div id="app"></div>
-  <?php wp_footer(); ?>
-</body>
-</html>
+<?php get_header(); ?>
+
+<div id="app"></div>
+
+<?php get_footer();
