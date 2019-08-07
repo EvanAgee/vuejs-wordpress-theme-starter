e02d1db4048693fb1aec0950eddac7d7e1496f49
Osama Abbas <osama@34ml.com>
2019-08-07 12:33:55 +0200

    Add tailwindcss and update packages

--- webpack.config.js
+++ webpack.config.js
@@ -16,16 +16,7 @@ module.exports = {
     rules: [
       {
         test: /\.vue$/,
-        loader: "vue-loader",
-        options: {
-          loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
-            scss: "vue-style-loader!css-loader!sass-loader",
-            sass: "vue-style-loader!css-loader!sass-loader?indentedSyntax"
-          }
-        }
+        loader: "vue-loader"
       },
       {
         test: /\.js$/,
