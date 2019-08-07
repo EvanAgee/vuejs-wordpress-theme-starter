14b6b6a85e3bf11a1e511ed99e879ff6f469eb3e
Osama Abbas <osama@34ml.com>
2019-08-07 12:43:41 +0200

    Extract sass/scss into seperate test

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,64 @@
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const webpack = require("webpack");
-const path = require("path");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
   entry: {
-    index: "./src/app.js",
-    "index.min": "./src/app.js"
+    index: './src/app.js',
+    'index.min': './src/app.js'
   },
   stats: { warnings: false }, // Hide warnings
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "scripts/[name].bundle.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'scripts/[name].bundle.js'
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: "vue-loader"
+        loader: 'vue-loader'
       },
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
             presets: [
               [
-                "env",
+                'env',
                 {
                   targets: {
                     browsers: [
-                      "Chrome >= 52",
-                      "FireFox >= 44",
-                      "Safari >= 7",
-                      "Explorer 11",
-                      "last 4 Edge versions"
+                      'Chrome >= 52',
+                      'FireFox >= 44',
+                      'Safari >= 7',
+                      'Explorer 11',
+                      'last 4 Edge versions'
                     ]
                   },
                   useBuiltIns: true
                 }
               ],
-              "stage-2"
+              'stage-2'
             ]
           }
         }
       },
       {
+        test: /\.s[ac]ss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
+          fallback: 'style-loader',
           use: [
-            { loader: "css-loader", options: { importLoaders: 1 } },
-            "postcss-loader"
+            { loader: 'css-loader', options: { importLoaders: 1 } },
+            'postcss-loader'
           ]
         })
       },
@@ -59,9 +66,9 @@ module.exports = {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "img/[name].[ext]"
+              name: 'img/[name].[ext]'
             }
           }
         ]
@@ -69,9 +76,9 @@ module.exports = {
       {
         test: /\.(png|woff|woff2|eot|ttf|svg)$/,
         use: {
-          loader: "file-loader",
+          loader: 'file-loader',
           options: {
-            name: "fonts/[name].[ext]"
+            name: 'fonts/[name].[ext]'
           }
         }
       }
@@ -79,12 +86,12 @@ module.exports = {
   },
   resolve: {
     alias: {
-      vue$: "vue/dist/vue.esm.js"
+      vue$: 'vue/dist/vue.esm.js'
     }
   },
   plugins: [
     new webpack.ProvidePlugin({
-      throttle: "lodash.throttle"
+      throttle: 'lodash.throttle'
     }),
     new webpack.optimize.UglifyJsPlugin({
       include: /\.min\.bundle\.js$/,
@@ -92,6 +99,6 @@ module.exports = {
       ecma: 8
     }),
     new webpack.LoaderOptionsPlugin({ minimize: true }),
-    new ExtractTextPlugin("styles.css")
+    new ExtractTextPlugin('styles.css')
   ]
 };
